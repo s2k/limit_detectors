46cf5360bf7d64cf457fd18aed07dbd2626e5daa
Stephan Kämper <the.tester@seasidetesting.com>
2014-04-11 20:29:26 +0200

    Follow Ruby coding convention

--- spec/limit_detectors_spec.rb
+++ spec/limit_detectors_spec.rb
@@ -24,16 +24,16 @@ describe '#at_most' do
   end
 
   it 'is true if all elements meet the criterion and the size is the given maximum number' do
-    expect( [1,1,1].at_most(3){|e| e == 1})
+    expect([1,1,1].at_most(3){|e| e == 1})
   end
 
   it 'is false if not enough elements meet the criterion' do
-    expect( [1, 2, 4].at_most(1){|e| e.even?}).to be_false
+    expect([1, 2, 4].at_most(1){|e| e.even?}).to be_false
   end
 
   it 'is true if 0 elements are expected to match' do
     r = Array.new(10){rand}
-    expect( r.at_most(0){ |i| i > 2 } ).to be_true
+    expect(r.at_most(0){ |i| i > 2 }).to be_true
   end
 
 end
