deaae128a813a4dee6c17737692f60639e3dfac7
Stephan Kämper <the.tester@seasidetesting.com>
2014-04-22 00:03:01 +0200

    Add source code documentation

--- lib/limit_detectors.rb
+++ lib/limit_detectors.rb
@@ -2,14 +2,20 @@ require 'limit_detectors/version'
 
 module LimitDetectors
 
+  # Check whether the condition given in the block
+  # occurs at most limit times in the collection
   def at_most(limit, &block)
     ocurrences_of(&block) <= limit
   end
 
+  # Check whether the condition given in the block
+  # occurs at least limit times in the collection
   def at_least(limit, &block)
     ocurrences_of(&block) >= limit
   end
 
+  # Count how often the condition given in the block
+  # is met for the collection
   def ocurrences_of &block
     inject(0) { |res, el|
       res += 1 if yield el
