0785731a38b7261b881a32a1c43ae251158b3627
Stephan Kämper <the.tester@seasidetesting.com>
2014-04-28 11:22:40 +0200

    Deprecate non-predicate method names

--- lib/limit_detectors.rb
+++ lib/limit_detectors.rb
@@ -2,15 +2,27 @@ require 'limit_detectors/version'
 
 module LimitDetectors
 
+  # Deprecated, use at_mmost? instead
+  def at_most(limit, &block)
+    Kernel.warn "'at_most' is deprecated, use 'at_most?' instead"
+    at_most? limit, &block
+  end
+
+  # Deprecated, use at_mmost? instead
+  def at_least(limit, &block)
+    Kernel.warn "'at_least' is deprecated, use 'at_least?' instead"
+    at_least? limit, &block
+  end
+
   # Check whether the condition given in the block
   # occurs at most limit times in the collection
-  def at_most(limit, &block)
+  def at_most?(limit, &block)
     ocurrences_of(&block) <= limit
   end
 
   # Check whether the condition given in the block
   # occurs at least limit times in the collection
-  def at_least(limit, &block)
+  def at_least?(limit, &block)
     ocurrences_of(&block) >= limit
   end
 
--- spec/limit_detectors_spec.rb
+++ spec/limit_detectors_spec.rb
@@ -5,31 +5,32 @@ Array.send :include, LimitDetectors
 describe '#at_most' do
 
   it 'is true for an empty Array' do
-    expect([].at_most(5){ true }).to be_true
+    expect(Kernel).to_not receive(:warn)
+    expect([].at_most?(5){ true }).to be_true
   end
 
   it 'is true if the criterion is met once' do
-    expect(["it's there"].at_most(1){ |el| el == "it's there"}).to be_true
+    expect(["it's there"].at_most?(1){ |el| el == "it's there"}).to be_true
   end
 
   it 'is true if all elements meet the criterion and the size is the given maximum number' do
-    expect([1,1,1].at_most(3){|e| e == 1})
+    expect([1,1,1].at_most?(3){|e| e == 1})
   end
 
   it 'is false if not enough elements meet the criterion' do
-    expect([1, 2, 4].at_most(1){|e| e.even?}).to be_false
+    expect([1, 2, 4].at_most?(1){|e| e.even?}).to be_false
   end
 
   it 'is true if 0 elements are expected to match' do
     r = Array.new(10){rand}
-    expect(r.at_most(0){ |i| i > 2 }).to be_true
+    expect(r.at_most?(0){ |i| i > 2 }).to be_true
   end
 
   describe 'Hash#at_most' do
     Hash.send :include, LimitDetectors
     it 'detects a condition based on key as well as value properties' do
       h = { 'foo' => 1, 'bar' => 4, 'baz' => 5, 'bum' => 1, 'fum' => 0}
-      expect( h.at_most(3){|ky,vl| ky.match(/^b/) || vl > 1  }).to be_true
+      expect( h.at_most?(3){|ky,vl| ky.match(/^b/) || vl > 1  }).to be_true
     end
   end
 
@@ -37,36 +38,37 @@ end
 
 describe '#at_least' do
   it 'is false for an empty Array' do
-    expect([].at_least(1){ true }).to be_false
-    expect([].at_least(1){ false }).to be_false
+    expect(Kernel).to_not receive(:warn)
+    expect([].at_least?(1){ true }).to be_false
+    expect([].at_least?(1){ false }).to be_false
   end
 
   it 'is true if the expected number is 0 and Array is empty' do
-    expect([].at_least(0){ true }).to be_true
-    expect([].at_least(0){ false }).to be_true
+    expect([].at_least?(0){ true }).to be_true
+    expect([].at_least?(0){ false }).to be_true
   end
 
   it 'is false if the container ist smaller than the expected number' do
     size = 10
-    expect(Array.new(10).at_least(size + 1){true}).to be_false
+    expect(Array.new(10).at_least?(size + 1){true}).to be_false
   end
 
   it 'is true if the criterion is met and expected once' do
-    expect(["it's there"].at_least(1){ |el| el == "it's there"}).to be_true
+    expect(["it's there"].at_least?(1){ |el| el == "it's there"}).to be_true
   end
 
   it 'is true if all elements meet the criterion and the size is the given minimum number' do
-    expect([1,1,1].at_least(3){|e| e == 1}).to be_true
+    expect([1,1,1].at_least?(3){|e| e == 1}).to be_true
   end
 
   it 'is true if enough elements meet the criterion' do
-    expect([1, 2, 4, 8].at_least(2){|e| e.even?}).to be_true
+    expect([1, 2, 4, 8].at_least?(2){|e| e.even?}).to be_true
   end
 
   it 'is true if there are enough elements to match' do
     r = Array.new(10){|i|i}
-    expect(r.at_least(7){ |i| i > 2 }).to be_true
-    expect(r.at_least(8){ |i| i > 2 }).to be_false
+    expect(r.at_least?(7){ |i| i > 2 }).to be_true
+    expect(r.at_least?(8){ |i| i > 2 }).to be_false
   end
 
 end
@@ -75,6 +77,18 @@ describe 'Using an object that doesn\'t respond to #inject will raise an excepti
   object = Object.new
   object.extend LimitDetectors
   it 'will raise an exception, if it\'s sent #atmost' do
-    expect{ object.at_most(1){ |el| el.condition? } }.to raise_exception(NoMethodError, /undefined method .inject./)
+    expect{ object.at_most?(1){ |el| el.condition? } }.to raise_exception(NoMethodError, /undefined method .inject./)
+  end
+end
+
+describe 'Give a warning, if non-predicate versions are used' do
+  it 'sends message Kernel.warn, if old-style method is used for at_most' do
+    expect(Kernel).to receive(:warn).with(/'at_most'.+deprecated.+'at_most\?'/)
+    [1,2,4,8].at_most(2) {|e| e.even?}
+  end
+
+  it 'sends message Kernel.warn, if old-style method is used for at_least' do
+    expect(Kernel).to receive(:warn).with(/'at_least'.+deprecated.+'at_least\?'/)
+    [1,2,4,8].at_least(2) {|e| e.even?}
   end
 end
