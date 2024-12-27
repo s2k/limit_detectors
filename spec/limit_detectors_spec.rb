2963779bdbd16bc08902f4af55ee20a7bee808d1
Stephan Kämper <the.tester@seasidetesting.com>
2024-12-27 16:04:02 +0100

    Updates Rubocop config & follows style guide (more)
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

--- spec/limit_detectors_spec.rb
+++ spec/limit_detectors_spec.rb
@@ -4,7 +4,7 @@ require 'set'
 
 Array.include LimitDetectors
 
-describe '#at_most' do
+describe Array, '#at_most?' do
   it 'is true for an empty Array' do
     expect(Kernel).not_to receive(:warn)
     expect([]).to be_at_most(5) { true }
@@ -26,20 +26,20 @@ describe '#at_most' do
   end
 
   it 'is true if 0 elements are expected to match' do
-    r = Array.new(10) { rand }
+    r = described_class.new(10) { rand }
     expect(r).to be_at_most(0) { |i| i > 2 }
-  end
-
-  describe Hash, '#at_most' do
-    described_class.include LimitDetectors
-    it 'detects a condition based on key as well as value properties' do
-      h = { 'foo' => 1, 'bar' => 4, 'baz' => 5, 'bum' => 1, 'fum' => 0 }
-      expect(h).to be_at_most(3) { |ky, vl| ky.match(/^b/) || vl > 1 }
-    end
   end
 end
 
-describe Array, '#at_least' do
+describe Hash, '#at_most?' do
+  described_class.include LimitDetectors
+  it 'detects a condition based on key as well as value properties' do
+    h = { 'foo' => 1, 'bar' => 4, 'baz' => 5, 'bum' => 1, 'fum' => 0 }
+    expect(h).to be_at_most(3) { |ky, vl| ky.match(/^b/) || vl > 1 }
+  end
+end
+
+describe Array, '#at_least?' do
   it 'is false for an empty Array, if at least one is expected' do
     expect(Kernel).not_to receive(:warn)
     [].at_least?(2) { |i| i > 2 }
@@ -102,9 +102,9 @@ describe LimitDetectors, '#ocurrences_of' do
     Set.include described_class
     subject { Set.new([1, 2, 3, 4, 5, 6, 7]) }
 
-    it('counts 3 even numbers')     { expect(subject.occurrences_of(&:even?)).to be 3 }
-    it('counts 4 odd numbers')      { expect(subject.occurrences_of(&:odd?)).to be 4 }
-    it('counts no number < 0')      { expect(subject.occurrences_of { |e| e < 0 }).to be 0 }
+    it('counts 3 even numbers') { expect(subject.occurrences_of(&:even?)).to be 3 }
+    it('counts 4 odd numbers') { expect(subject.occurrences_of(&:odd?)).to be 4 }
+    it('counts no number < 0') { expect(subject.occurrences_of { |e| e < 0 }).to be 0 }
     it('counts 7 positive numbers') { expect(subject.occurrences_of { |e| e > 0 }).to be 7 }
   end
 
